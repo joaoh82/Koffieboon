31a6bc93c85a9a4b060b60dde6f8ba3c8c27a695
João Henrique Machado Silva <joaoh82@gmail.com>
2025-02-21 10:10:42 +0100

    code cleanup

--- Engine/src/Koffieboon.h
+++ Engine/src/Koffieboon.h
@@ -3,7 +3,6 @@
 // For use by Koffieboon applications
 
 #include "Koffieboon/Application.h"
-#include "Koffieboon/Core.h"
 #include "Koffieboon/Defines.h"
 #include "Koffieboon/core/Logger.h"
 #include "Koffieboon/core/Asserts.h"
